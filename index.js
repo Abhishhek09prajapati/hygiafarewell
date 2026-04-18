ca984a9ee61b006692e763bdc973fcf719de731e
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-04-18 14:20:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+fetch("https://opensheet.elk.sh/1RA-KlGO642ji9gQMH4J15P_8YIkLRYUOGymUR5D8Kz4/Sheet1")
+    .then(response => response.json())
+    .then(data => {
+
+
+        setInterval(() => {
+            var i = Math.floor(Math.random() * data.length);
+            document.querySelector(".markquee").innerText = data[i].message;
+            document.querySelector(".farewellimage").src = `/images/${data[i].teacher}`;
+            
+        },2200);
+
+
+
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+
+
+function goto1() {
+    window.open("https://wa.me/916387215755", "_blank");
+}
